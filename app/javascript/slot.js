008d47678a2d58236fd4fad809008a764a96b923
atomi3973 <atomiroi3973@gmail.com>
2025-09-18 23:05:58 +0900

    音追加

--- app/javascript/slot.js
+++ app/javascript/slot.js
@@ -2,11 +2,19 @@ document.addEventListener("turbo:load", () => {
   const slotMachine = document.getElementById("slot-machine");
   if (!slotMachine) return; //安全のため、Domが生成しない可能性もある
 
+  const spinSound = new Audio("/sounds/spin.wav");
+  const stopSound = new Audio("/sounds/stop.mp3");
+  const winSound = new Audio("/sounds/win.wav");
+  const shineSound = new Audio("/sounds/shine.mp3");
+
   const slots = slotMachine.querySelectorAll(".slot");
   const resultIcon = slotMachine.dataset.resultIcon; // データ属性から取得
   const icons = ["🌕", "🔥", "🌰", "🍂", "🎃", "💀"];
 
   let counter = 0;
+  spinSound.volume = 0.4;
+  spinSound.play();
+
   const interval = setInterval(() => {
     slots.forEach(slot => {
       slot.textContent = icons[Math.floor(Math.random() * icons.length)];// 少数✖️配列長さでランダムにしている、rubyではsampleがあるのにないっぽい
@@ -18,6 +26,17 @@ document.addEventListener("turbo:load", () => {
       slots.forEach(slot => slot.textContent = resultIcon); // 絵文字を揃える
       document.getElementById("fortune-title").classList.remove("hidden");
       document.getElementById("fortune-desc").classList.remove("hidden");
+      spinSound.pause();
+      spinSound.currentTime = 0;
+      if (resultIcon !== "🌕") {
+        stopSound.play();
+      } else {
+        winSound.play();
+        setTimeout(() => {
+          shineSound.play();
+        }, 7000);
+      }
+      
     }
   }, 100);
 });
